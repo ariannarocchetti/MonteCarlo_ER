ad90caeb10f1625bbd64ca5d87b410c7ee30d190
Arianna Rocchetti <arocchetti@pcfr31.physik.uni-freiburg.de>
2019-11-07 18:36:54 +0100

    order

--- batch/read_nSort.py
+++ batch/read_nSort.py
@@ -5,46 +5,40 @@ import os
 from datetime import date
 
 ##### INPUT PARAMETER #####
+material_array = ["SS_OuterCryostat"]
 
 material_array = ["SS_OuterCryostat",
                 "SS_InnerCryostat",
-                "OuterCryostatReflector",
-                "SS_BellPlate",
-                "SS_BellSideWall",
-                "PmtTpc",
+                "OuterCryostatReflector", 
+                "SS_BellPlate", 
+                "SS_BellSideWall", 
+                "PmtTpc", 
                 "Copper_TopRing",
-                "Copper_LowerRing",
-                "Teflon_Pillar_",
-                "Copper_FieldGuard_",
-                "Copper_FieldShaperRing_",
-                "SS_GateRing",
+                "Copper_LowerRing", 
+                "Teflon_Pillar_", 
+                "Copper_FieldGuard_", 
+                "Copper_FieldShaperRing_", 
+                "SS_GateRing", 
                 "SS_AnodeRing",
                 "SS_TopMeshRing",
-                "SS_CathodeRing",
-                "SS_BottomMeshRing",
-                "Teflon_BottomTPC",
-                "Teflon_TPC",
-                "GXeTeflon_TopElectrodesFrame",
-                "Teflon_TopElectrodesFrame",
-                "Copper_BottomPmtPlate",
-                "Copper_TopPmtPlate",
+               "SS_CathodeRing", 
+                "SS_BottomMeshRing", 
+                "Teflon_BottomTPC", 
+                "Teflon_TPC", ""
                 ]
 
-isotope_array = ["U238",
-                "Co60",
-                "K40",
-                "Cs137",
-                "Th228",
-                "U235",
-                "Th232",
-                "Ra226",
-                #"geantinos"
-                ]
+isotope_array = ["U238Pb206", 
+                "Co60", 
+                "K40", 
+                "Cs137", 
+                "Th228Pb208", 
+                "U235Pb207"]
+
 #isotope_array = ["geantinos"]
-EVENT_COUNT = 100000
+EVENT_COUNT = 10000
 
 DATE_STRING = str(date.today())
-#DATE_STRING = "2019-11-04"
+#DATE_STRING = "2019-10-14"
 counter = 0
 file_corrupted = 0
 file_ok = 0
@@ -57,13 +51,13 @@ for MATERIAL_STRING in material_array:
         
         for file in os.listdir(PATH):
             
-            if file.endswith(".out"):
+            if file.endswith("_Sort.out"):
                 
                 counter = counter +1
                 path_file = os.path.join(PATH, file)
                 fp = open(path_file, 'r')
                 
-                if "fileMerger::CleanUp() Done" in fp.read():
+                if "Events written: " in fp.read():
                     file_ok = file_ok +1
                     print(" ---> ", path_file, "----> OK!!")
                     fp.close()
