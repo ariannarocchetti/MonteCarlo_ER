4c63890f864c2d00a1b2d7704bd966b818a2236e
ariannarocchetti <arianna.rocchetti@physik.uni-freiburg.de>
2019-12-16 01:16:08 -0600

    new scripts for managing data

--- nSort_stage.py
+++ nSort_stage.py
@@ -8,6 +8,27 @@ import glob
 
 import subprocess
 from pexpect import pxssh
+import sys 
+
+dir_ =sys.argv[1] #"20191202T162310-0600"
+print(dir_)
+
+
+
+#path =  "/dali/lgrandi/xenonnt/simulations/er_simulations/raw_files/"
+dir_to_sort = dir_
+
+list_to_sort = glob.glob("%s/*10.root"%dir_to_sort)
+    #print(list_to_sort[0:2])
+    #print(list_to_sort[-4:-2])
+for j in list_to_sort:
+    k = os.path.splitext("%s"%j)[0]
+    os.system("./nSort -i  %s -d XENONnT 2 1 0 0 0"%k)
+        #print("./nSort -i  %s -d XENONnT 2 1 0 0 0"%k)
+#print(len(list_to_sort))
+print("------------------")
+
+"""
 
 storage_path = "/dali/lgrandi/xenonnt/simulations/er_simulations"
 command = ("ls -d %s/raw_files/*"%storage_path)
@@ -16,7 +37,7 @@ list_=subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
 out = out.decode("utf-8")
 out = out.split("\n")
 
-
+print(out)
 
 for i in range(0, len(out)-2):
 
@@ -29,5 +50,5 @@ for i in range(0, len(out)-2):
         #print("./nSort -i  %s -d XENONnT 2 1 0 0 0"%k)
     print(len(list_to_sort))
     print("------------------")
-    
+"""    
 
